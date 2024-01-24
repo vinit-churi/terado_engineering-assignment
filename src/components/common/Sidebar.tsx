7694ce2c39c45b2400a678eba00af9634ab27dc8
Vinit Churi <vinitchuri@hotmail.com>
2024-01-25 01:02:08 +0530

    Remove unused imports and state variable from Sidebar component

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -2,11 +2,7 @@ import { Link } from "react-router-dom";
 import logo from "@/assets/images/logo.png";
 import sidebarLinks from "@/constants/sidebarLinks";
 import SidebarLink from "@/components/common/SidebarLink";
-import { MdOutlineWbSunny } from "react-icons/md";
-import { FaRegMoon } from "react-icons/fa";
-import { useState } from "react";
 const Sidebar = () => {
-  const [theme, setTheme] = useState(false);
   return (
     <div className="bg-blue-50/30 row-start-1 row-span-2 border-r border-slate-300/40 col-start-1 col-span-1 px-6 py-6">
       <Link to="/" className="flex items-center">
